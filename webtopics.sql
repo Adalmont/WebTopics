391f17ce87901411c2343df101d208941bd072c0
Adalmont <adalmont22@gmail.com>
2017-11-19 19:01:54 +0100

    Actualizados war y base de datos

--- webtopics.sql
+++ webtopics.sql
@@ -63,15 +63,16 @@ CREATE TABLE IF NOT EXISTS `mensajes` (
   KEY `FK_mensaje-usuario` (`idUsuario`),
   CONSTRAINT `FK_mensaje-tema` FOREIGN KEY (`idTema`) REFERENCES `temas` (`idTema`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_mensaje-usuario` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla webtopics.mensajes: ~3 rows (aproximadamente)
+-- Volcando datos para la tabla webtopics.mensajes: ~4 rows (aproximadamente)
 DELETE FROM `mensajes`;
 /*!40000 ALTER TABLE `mensajes` DISABLE KEYS */;
 INSERT INTO `mensajes` (`idMensaje`, `idTema`, `idUsuario`, `fechaCreacion`, `contenido`) VALUES
 	(1, 7, 3, '2017-11-04 18:00:33', 'Mensaje de prueba'),
 	(2, 8, 3, '2017-11-07 23:06:04', 'asdasda'),
-	(3, 8, 3, '2017-11-07 23:06:12', 'dwedwedew');
+	(3, 8, 3, '2017-11-07 23:06:12', 'dwedwedew'),
+	(4, 1, 4, '2017-11-08 19:19:09', 'Mensaje');
 /*!40000 ALTER TABLE `mensajes` ENABLE KEYS */;
 
 -- Volcando estructura para tabla webtopics.privados
@@ -81,6 +82,8 @@ CREATE TABLE IF NOT EXISTS `privados` (
   `idReceptor` int(11) NOT NULL,
   `titulo` varchar(50) NOT NULL DEFAULT 'Mensaje Privado',
   `contenido` varchar(250) NOT NULL,
+  `fechaCreacion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `leido` set('s','n') NOT NULL DEFAULT 's',
   PRIMARY KEY (`idPrivado`),
   KEY `FK_privado-creador` (`idCreador`),
   KEY `FK_privado-receptor` (`idReceptor`),
@@ -107,9 +110,9 @@ CREATE TABLE IF NOT EXISTS `temas` (
   KEY `FK_tema-categoria` (`idCategoria`),
   CONSTRAINT `FK_tema-categoria` FOREIGN KEY (`idCategoria`) REFERENCES `categorias` (`idCategoria`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_tema-usuario` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla webtopics.temas: ~8 rows (aproximadamente)
+-- Volcando datos para la tabla webtopics.temas: ~9 rows (aproximadamente)
 DELETE FROM `temas`;
 /*!40000 ALTER TABLE `temas` DISABLE KEYS */;
 INSERT INTO `temas` (`idTema`, `idUsuario`, `idCategoria`, `titulo`, `fechaCreacion`, `mensajeInicial`, `estado`) VALUES
@@ -120,7 +123,8 @@ INSERT INTO `temas` (`idTema`, `idUsuario`, `idCategoria`, `titulo`, `fechaCreac
 	(5, 3, 5, 'prueba Miscelanea', '2017-10-09 23:27:06', 'prueba de tema', 'a'),
 	(6, 3, 1, 'zczxczc', '2017-11-03 18:25:33', 'xczczxcz', 'a'),
 	(7, 3, 4, 'Prueba de Tema Larga', '2017-11-04 18:00:14', 'Esta es una prueba de como se deberia de ver un tea bien construido', 'a'),
-	(8, 3, 1, 'wdew', '2017-11-07 22:57:23', 'ewdwed', 'a');
+	(8, 3, 1, 'wdew', '2017-11-07 22:57:23', 'ewdwed', 'a'),
+	(9, 4, 1, 'Picaporte', '2017-11-08 19:19:56', 'Tenedor', 'a');
 /*!40000 ALTER TABLE `temas` ENABLE KEYS */;
 
 -- Volcando estructura para tabla webtopics.usuarios
@@ -137,13 +141,14 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   PRIMARY KEY (`idUsuario`),
   UNIQUE KEY `apodo` (`apodo`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- Volcando datos para la tabla webtopics.usuarios: ~0 rows (aproximadamente)
 DELETE FROM `usuarios`;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
 INSERT INTO `usuarios` (`idUsuario`, `nombre`, `apellidos`, `apodo`, `email`, `tipo`, `clave`, `avatar`, `fechaAlta`) VALUES
-	(3, 'Prueba', 'Prueba', 'Prueba', 'prueba@prueba.com', 'e', 'yJO61okntFfb7TlGDmr9Yg==', '3.jpg', '2017-10-01 19:00:12');
+	(3, 'Prueba', 'Prueba', 'Prueba', 'prueba@prueba.com', 'e', 'yJO61okntFfb7TlGDmr9Yg==', '3.jpg', '2017-10-01 19:00:12'),
+	(4, 'Paul', 'McCartney', 'Pmc', 'paul@mail.com', 'e', 'bGMhKrSOhAHq9rWbldgWqQ==', '4.jpg', '2017-11-08 19:15:30');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
